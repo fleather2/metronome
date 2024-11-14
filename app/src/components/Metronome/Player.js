3b20c5a1f5999e970b517a5d898d1ea1796d2300
fleather2 <fleather2@users.noreply.github.com>
2024-11-14 17:03:32 -0500

    Adding buttons

--- app/src/components/Metronome/Player.js
+++ app/src/components/Metronome/Player.js
@@ -6,6 +6,7 @@ import "./beat.css";
 const Player = ({bpm, numBeats, audioContext, gainNode, beatSet}) => {
     const hasPageBeenRendered = useRef(false);
     const [beatList, setBeatList] = useState([]);
+    const [play, setPlay] = useState(false);
     
     useEffect( () => {
         console.log(beatList);
@@ -13,45 +14,52 @@ const Player = ({bpm, numBeats, audioContext, gainNode, beatSet}) => {
 
     let intervalDuration = 0.9;
     useEffect( () => {
-        const intervalId = setInterval( () => {
-            const startTime = audioContext.currentTime;
-            
-            const noteInterval = 60/bpm;
-            let nextNoteTime = noteInterval*(Math.floor(audioContext.currentTime/noteInterval)+1);
+        if (play) {
+            const intervalId = setInterval( () => {
+                const startTime = audioContext.currentTime;
+                const noteInterval = 60/bpm;
+                let nextNoteTime = noteInterval*(Math.floor(audioContext.currentTime/noteInterval)+1);
 
-            while (nextNoteTime <= startTime + intervalDuration) {
-                audioContext.resume();
-                console.log("Scheduling note for", nextNoteTime);
-                const oscNode = audioContext.createOscillator();
-                oscNode.connect(gainNode);
+                while (nextNoteTime <= startTime + intervalDuration) {
+                    audioContext.resume();
+                    console.log("Scheduling note for", nextNoteTime);
+                    const oscNode = audioContext.createOscillator();
+                    oscNode.connect(gainNode);
 
-                // Emphasize strong beats
-                const b = Math.round(nextNoteTime / noteInterval);
-                console.log("Beat num", b);
-                if (b % numBeats === 0) {
-                    oscNode.frequency.value = 1000;
-                } else {
-                    oscNode.frequency.value = 800;
-                }
+                    // Emphasize strong beats
+                    const b = Math.round(nextNoteTime / noteInterval);
+                    console.log("Beat num", b);
+                    if (b % numBeats === 0) {
+                        oscNode.frequency.value = 1000;
+                    } else {
+                        oscNode.frequency.value = 800;
+                    }
 
-                oscNode.start(nextNoteTime);
-                oscNode.stop(nextNoteTime + 0.05);
-                const beat = {
-                    beatTime: nextNoteTime,
-                    beatNum: b
-                }
-                setBeatList((prevArray) => {
-                    const newArray = [...prevArray, beat];
-                    return newArray;
-                });
-                nextNoteTime += noteInterval;
-            } 
-        }, intervalDuration*1000)
-        return () => {
-            clearInterval(intervalId);
+                    oscNode.start(nextNoteTime);
+                    oscNode.stop(nextNoteTime + 0.05);
+                    const beat = {
+                        beatTime: nextNoteTime,
+                        beatNum: b
+                    }
+                    setBeatList((prevArray) => {
+                        const newArray = [...prevArray, beat];
+                        return newArray;
+                    });
+                    nextNoteTime += noteInterval;
+                } 
+            }, intervalDuration*1000)
+            return () => {
+                clearInterval(intervalId);
+            }
         }
-    }, [bpm])
+    }, [bpm, play])
 
+    return (
+        <Container>
+            <Button onClick={() => setPlay(true)}>Start</Button>
+            <Button onClick={() => setPlay(false)}>Stop</Button>
+        </Container>
+    )
 }
 
 export default Player;
