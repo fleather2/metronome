daaa5872e693499b9b70619a7568593f4c6a69ca
fleather2 <fleather2@users.noreply.github.com>
2024-11-17 17:02:12 -0500

    Potentially a working beat update thing

--- app/src/components/Metronome/Player.js
+++ app/src/components/Metronome/Player.js
@@ -1,16 +1,16 @@
 import { useState, useEffect, useRef} from "react";
 import { animate } from "motion";
 import { Container, Button } from "@mui/material";
-import "./beat.css";
+import Dots from "./Dots";
+
+function timeout(delay) {
+    return new Promise( res => setTimeout(res, delay) );
+}
 
 const Player = ({bpm, numBeats, audioContext, gainNode, beatSet}) => {
     const hasPageBeenRendered = useRef(false);
     const [beatList, setBeatList] = useState([]);
     const [play, setPlay] = useState(false);
-    
-    useEffect( () => {
-        console.log(beatList);
-    }, [beatList])
 
     let intervalDuration = 0.9;
     useEffect( () => {
@@ -54,6 +54,7 @@ const Player = ({bpm, numBeats, audioContext, gainNode, beatSet}) => {
                         return newArray;
                     });
                     nextNoteTime += noteInterval;
+                    scheduleBeatChange(beat);
                 } 
             }, intervalDuration*1000)
             return () => {
@@ -62,26 +63,80 @@ const Player = ({bpm, numBeats, audioContext, gainNode, beatSet}) => {
         }
     }, [bpm, play])
 
-    const [currentBeat, setCurrentBeat] = useState(0);
-    const [nextBeat, setNextBeat] = useState(0);
-    useEffect( () => {
-        const intervalId = setInterval( () => {
-            const startTime = audioContext.currentTime;
-            let a = beatList.find(({beatTime}) => beatTime > startTime);
-            if (a) {
-                while (audioContext.currentTime < a.beatTime) {
-                    setCurrentBeat(a.beatNum);
-                }
-            }
-        }, 10);
-    })
+    function scheduleBeatChange(beat) {
+        if (play) {
+            console.log(beat, (beat.beatTime - audioContext.currentTime)*1000);
+            setTimeout( () => {
+                setCurrentBeat(beat.beatNum);
+            }, (beat.beatTime - audioContext.currentTime)*1000)
+        }
+    }
 
-    useEffect( () => {})
+
+    useEffect( () => {
+        //console.log(beatList)
+    }, [beatList])
+    const [currentBeat, setCurrentBeat] = useState(0);
+    const [nextBeat, setNextBeat] = useState();
+
+    // useEffect ( () => {
+    //     if (play) {
+    //         let bl = beatList;
+    //         for (let i = 0; i < bl.length; i++) {
+    //             const startTime = audioContext.currentTime;
+    //             console.log(i);
+    //             if (bl.at(i).beatTime < audioContext.beatTime) {
+    //                 setCurrentBeat(i);
+    //             }
+    //         }
+    //     }
+    // }, [beatList]);
+    const [lastAudioContextTime, setLastAudioContextTime] = useState(0.0);
+    useEffect( () => {
+        //console.log(audioContext.currentTime);
+        setLastAudioContextTime(audioContext.currentTime);
+    }, [audioContext.currentTime])
+
+ 
+    // useEffect( () => {
+    //     if (play) {
+    //         const intervalId = setInterval( () => {
+    //             const startTime = audioContext.currentTime;
+    //             let nextBeatIdx = beatList.findIndex(({beatTime}) => beatTime > startTime);
+    //             if (nextBeatIdx != -1) {
+    //                 setNextBeat(beatList[nextBeatIdx]);
+    //                 beatList.splice(0, nextBeatIdx);
+    //                 setBeatList(beatList);
+    //             }
+    //         }, 10);
+    //     }
+    // })
+
+    // useEffect( () => {
+    //     if (nextBeat) {
+    //         console.log("Waiting to update beat", nextBeat);
+    //         while (audioContext.currentTime < nextBeat.beatTime) {
+    //         }
+    //         setCurrentBeat(nextBeat.beatNum);
+    //     }
+    // }, [nextBeat])
+
+    function startMetronome() {
+        audioContext.suspend();
+        setPlay(true);
+    }
+
+    function stopMetronome() {
+        audioContext.resume();
+        setPlay(false);
+    }
 
     return (
         <Container>
-            <Button onClick={() => setPlay(true)}>Start</Button>
-            <Button onClick={() => setPlay(false)}>Stop</Button>
+            <Button variant="contained" onClick={() => startMetronome()}>Start</Button>
+            <Button variant="contained" onClick={() => stopMetronome()}>Stop</Button>
+            <Dots numBeats={numBeats} currentBeat={currentBeat}/>
+            
             {currentBeat}
         </Container>
     )
