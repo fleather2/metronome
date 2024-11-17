870adb523560bd81a0257824ba5772d284609d1f
fleather2 <fleather2@users.noreply.github.com>
2024-11-17 18:02:54 -0500

    Very basi beat circles working

--- app/src/components/Metronome/BeatCircles.js
+++ app/src/components/Metronome/BeatCircles.js
@@ -1,8 +1,26 @@
 import { Container } from "@mui/material";
+import "./beat.css";
+
+function BeatCircle (isCurrent) {
+    console.log(isCurrent)
+    if (isCurrent) {
+        return (<div className="beatCircleFill"/>)
+    } else{
+        return (<div className="beatCircle"/>)
+    }
+}
 
 function BeatCircles ({numBeats, currentBeat}) {
+    let circles = [];
+    for (let i = 0; i < numBeats; i++) {
+        circles.push(BeatCircle(currentBeat % numBeats == i));
+    }
+
     return (
-        <Container>{currentBeat}</Container>
+        <Container>
+            {currentBeat}
+            {circles}
+        </Container>
     )
 }
 
